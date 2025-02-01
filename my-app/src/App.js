b11c9d361eed7041d49c267c03b4ce6f1746a068
delaram <delaram.bahreiniesfa@ucalgary.ca>
2025-02-01 15:49:47 -0700

    new files added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,46 @@
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import LeftBar from "./components/leftBar/LeftBar";
+import RightBar from "./components/rightBar/RightBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"; 
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <div style={{ display: "flex" }}>
+        <LeftBar />
+        <Outlet /> {/* This allows nested routes to render */}
+        <RightBar />
+      </div>
+    </div>
+  );
+};
 
 const router = createBrowserRouter(
   [
-    { path: "/", element: <Login /> }, // Redirects "/" to login
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Profile />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Home />,
+        },
+      ],
+    },
     { path: "/login", element: <Login /> },
     { path: "/register", element: <Register /> },
     { path: "*", element: <div>404 - Page Not Found</div> },
   ],
-  { basename: "/SocialMedia.github.io" } // 👈 Add this if hosted on GitHub Pages
+  { basename: "/SocialMedia.github.io" } // 👈 This helps with GitHub Pages deployment
 );
 
 function App() {
