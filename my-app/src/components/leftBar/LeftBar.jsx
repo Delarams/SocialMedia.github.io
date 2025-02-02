42eb4921c7f066257df343efa8249d2963254b64
delaram <delaram.bahreiniesfa@ucalgary.ca>
2025-02-02 13:10:28 -0700

    Updated LeftBar component and added new assets

--- my-app/src/components/leftBar/LeftBar.jsx
+++ my-app/src/components/leftBar/LeftBar.jsx
@@ -2,7 +2,18 @@ import "./leftBar.scss";
 
 const LeftBar = () => {
   return (
-    <div className="leftBar">LeftBar</div>
+    <div className="leftBar">
+      <div className="menu">
+        <div className="user">
+          <img src="https://images.pexels.com/photos/10727250/pexels-photo-10727250.jpeg" alt="" />
+          <span>John Doe</span>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <span>Friends</span>
+        </div>
+      </div>
+    </div>
   );
 };
 
