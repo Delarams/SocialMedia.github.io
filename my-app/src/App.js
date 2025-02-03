c8cce7aeb458ad9e9861de9f0999c772192b0a46
delaram <delaram.bahreiniesfa@ucalgary.ca>
2025-02-02 20:04:39 -0700

    Added global styling in style.scss and initialized changes for dark mode

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,13 +5,14 @@ import LeftBar from "./components/leftBar/LeftBar";
 import RightBar from "./components/rightBar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
+import "./style.scss"
 import { createBrowserRouter, RouterProvider, Outlet, Navigate } from "react-router-dom"; // ✅ Ensure Outlet is imported
 
 const currentUser = true;
 
 const Layout = () => {
   return (
-    <div>
+    <div className="theme-dark">
       <Navbar />
       <div style={{ display: "flex" }}>
         <LeftBar />
--- my-app/src/components/rightBar/RightBar.jsx
+++ my-app/src/components/rightBar/RightBar.jsx
@@ -27,6 +27,83 @@ const RightBar = () => {
             </div>
           </div>
         </div>
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/10727250/pexels-photo-10727250.jpeg" alt="" />
+              <p>
+              <span>Jane Doe</span>  changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/10727250/pexels-photo-10727250.jpeg" alt="" />
+              <p>
+              <span>Jane Doe</span>  changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/10727250/pexels-photo-10727250.jpeg" alt="" />
+              <p>
+              <span>Jane Doe</span>  changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/10727250/pexels-photo-10727250.jpeg" alt="" />
+              <p>
+              <span>Jane Doe</span>  changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/10727250/pexels-photo-10727250.jpeg" alt="" />
+              <div className="online"/>
+              <span>Jane Doe</span> 
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/10727250/pexels-photo-10727250.jpeg" alt="" />
+              <div className="online"/>
+              <span>Jane Doe</span> 
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/10727250/pexels-photo-10727250.jpeg" alt="" />
+              <div className="online"/>
+              <span>Jane Doe</span> 
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/10727250/pexels-photo-10727250.jpeg" alt="" />
+              <div className="online"/>
+              <span>Jane Doe</span> 
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/10727250/pexels-photo-10727250.jpeg" alt="" />
+              <div className="online"/>
+              <span>Jane Doe</span> 
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
