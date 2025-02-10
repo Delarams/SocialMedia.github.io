22c48a34dcc240e0a7d5e6f693794b435771ded9
delaram <delaram.bahreiniesfa@ucalgary.ca>
2025-02-09 23:43:49 -0700

    Updated post controller and routes, added moment package

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,6 +1,6 @@
 import {db} from "../connect.js"
 import jwt from "jsonwebtoken";
-// import "./controllers/auth.js"
+import moment from "moment";
 
 
 export const getPosts =(req, res)=>{
@@ -12,7 +12,36 @@ export const getPosts =(req, res)=>{
 
         // AS p JOIN users AS u because we want fetch user data from user table to show the user 
         //profile pic on top of the post
-        const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ?`
+        const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ? ORDER BY p.createdAt DESC `
+
+        db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+            if (err) return res.status(500).json(err);
+            return res.status(200).json(data)
+        });
+    });
+
+
+    
+}
+
+
+export const addPost =(req, res)=>{
+    const token = req.cookies.accessToken;
+    if(!token) return res.status(401).json("Not logged in!")
+
+    jwt.verify(token, "secretkey", (err, userInfo)=>{
+        if(err) return res.status(403).json("Token is not valid!")
+
+        // AS p JOIN users AS u because we want fetch user data from user table to show the user 
+        //profile pic on top of the post
+        const q = `INSERT INTO posts ("desc", "img", "createdAt", "userId") VALUES ?`
+
+        const values = [
+            req.body.desc, 
+            req.body.img, 
+            moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+            userInfo.id
+        ]
 
         db.query(q, [userInfo.id, userInfo.id], (err, data) => {
             if (err) return res.status(500).json(err);
